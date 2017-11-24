ef84b695b659d6fc02a9ab419d17bec7e5b6f541
Tuomas Puro <tuomas.puro@esedulainen.fi>
2017-11-24 14:33:29 +0200

    Navigoinnin menu-togglet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-// Tänne projektin javascripti
+function blockToggle(blockId) {
+    console.log(blockId);
+    var x = document.getElementById(blockId);
+    console.log(x);
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function childToggle(x){
+    var subMenu = x.nextSibling.nextSibling;
+    console.log(subMenu.style.display);
+    if (subMenu.style.display != 'block') {
+        subMenu.style.display = 'block';
+    } else {
+        subMenu.style.display = 'none';
+    }
+    
+}
